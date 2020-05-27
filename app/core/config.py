dfe5b0a66ddad2f047012d1f0b61f7d33ea0c0a4
rednafi <redowan.nafi@gmail.com>
2020-05-27 19:48:36 +0600

    Added tests

---
+++ app/core/config.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_username = os.environ.get("API_USERNAME")
+api_password = os.environ.get("API_PASSWORD")
--- app/routes/views.py
+++ app/routes/views.py
@@ -1,15 +1,33 @@
-from fastapi import APIRouter
+import secrets
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from app.core import config
 from app.api_a.mainmod import func_main as func_main_a
 from app.api_b.mainmod import func_main as func_main_b
 
 router = APIRouter()
+security = HTTPBasic()
 
 
-@router.get("/api-a/{num}", tags=["users"])
-async def views_a(num: int):
-    return func_main_a(num)
+def authorize(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, config.api_username)
+    correct_password = secrets.compare_digest(credentials.password, config.api_password)
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return True
 
 
-@router.get("/api-b/{num}", tags=["users"])
-async def views_a(num: int):
-    return func_main_b(num)
+@router.get("/api-a/{num}", tags=["api_a"])
+async def views_a(num: int, auth=Depends(authorize)):
+    if auth is True:
+        return func_main_a(num)
+
+
+@router.get("/api-b/{num}", tags=["api_b"])
+async def views_a(num: int, auth=Depends(authorize)):
+    if auth is True:
+        return func_main_b(num)
