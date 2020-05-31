17dec02da446df83cea1b222e5e3e925a7b06c61
rednafi <redowan.nafi@gmail.com>
2020-06-01 01:35:11 +0600

    Config deps injection

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,52 @@
-import os
+from dataclasses import dataclass
+from os import environ
+from typing import Optional
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-api_username = os.environ.get("API_USERNAME")
-api_password = os.environ.get("API_PASSWORD")
+
+@dataclass
+class BaseConfig:
+    """Common configurations."""
+
+    HOST: Optional[str] = environ.get("HOST")
+
+    @staticmethod
+    def typecast(variable, type_):
+        if variable is not None:
+            return type_(variable)
+
+
+@dataclass
+class DevConfig(BaseConfig):
+    """Development configurations."""
+
+    API_USERNAME: Optional[str] = environ.get("DEV.API_USERNAME")
+    API_PASSWORD: Optional[str] = environ.get("DEV.API_PASSWORD")
+
+
+@dataclass
+class ProdConfig(BaseConfig):
+    """Production configurations."""
+
+    API_USERNAME: Optional[str] = environ.get("PROD.API_USERNAME")
+    API_PASSWORD: Optional[str] = environ.get("PROD.API_PASSWORD")
+
+
+class Config:
+    """Configuration factory class."""
+
+    def __init__(self, env: Optional[str]):
+        self.env = env
+
+    def __call__(self):
+        if self.env == "dev":
+            return DevConfig()
+
+        elif self.env == "prod":
+            return ProdConfig()
+
+
+config = Config(env=environ.get("ENVIRONMENT"))()
--- app/routes/views.py
+++ app/routes/views.py
@@ -6,15 +6,15 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 from app.api_a.mainmod import main_func as main_func_a
 from app.api_b.mainmod import main_func as main_func_b
-from app.core import config
+from app.core.config import config
 
 router = APIRouter()
 security = HTTPBasic()
 
 
 def authorize(credentials: HTTPBasicCredentials = Depends(security)) -> bool:
-    correct_username = secrets.compare_digest(credentials.username, config.api_username)
-    correct_password = secrets.compare_digest(credentials.password, config.api_password)
+    correct_username = secrets.compare_digest(credentials.username, config.API_USERNAME)
+    correct_password = secrets.compare_digest(credentials.password, config.API_PASSWORD)
     if not (correct_username and correct_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
