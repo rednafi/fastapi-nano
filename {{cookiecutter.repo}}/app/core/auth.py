ed76ad00e256833efe29fd4f9f8383eabba21a20
rednafi <redowan.nafi@gmail.com>
2020-06-21 18:20:49 +0600

    OAuth2 based authorization

--- {{cookiecutter.repo}}/app/core/auth.py
+++ {{cookiecutter.repo}}/app/core/auth.py
@@ -12,14 +12,11 @@ from app.core.config import config
 # openssl rand -hex 32
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 5256000000 # infinity
+ACCESS_TOKEN_EXPIRE_MINUTES = 5256000000  # infinity
 
 
 fake_users_db = {
-    "ubuntu": {
-        "username": config.API_USERNAME,
-        "hashed_password": config.API_PASSWORD,
-    }
+    "ubuntu": {"username": config.API_USERNAME, "hashed_password": config.API_PASSWORD,}
 }
 
 
