3814c653a4d2a47143b17be96d21478cb2ed6232
Redowan Delowar <redowan.nafi@gmail.com>
2021-09-14 04:43:15 +0600

    Updated ci

--- scripts/docker_ci.sh
+++ scripts/docker_ci.sh
@@ -6,12 +6,15 @@
 # Turn on bash strict mode.
 set -euxo pipefail
 
+current_dir=$(pwd)
+cookiecutter_dir=\{\{cookiecutter.repo\}\}
+
 # Install the dependencies.
-pip install -r \{\{cookiecutter.repo\}\}/requirements-dev.txt &&\
-pip install -r \{\{cookiecutter.repo\}\}/requirements.txt
+pip install -r $cookiecutter_dir/requirements.txt  &&\
+pip install -r $cookiecutter_dir/requirements.txt
 
 # Create a concrete project from the cookiecutter template.
-cookiecutter $(pwd) --no-input -f
+cookiecutter $current_dir --no-input -f
 
 # Run the tests.
 pytest fastapi-nano
@@ -21,7 +24,7 @@ docker-compose -f fastapi-nano/docker-compose.yml up --build -d
 
 # Cleanup.
 docker-compose -f fastapi-nano/docker-compose.yml down
-rm -rf fastapi-nano
+## rm -rf fastapi-nano
 
 # turn off command echo
 set +x
--- scripts/update_deps.sh
+++ scripts/update_deps.sh
@@ -5,14 +5,17 @@
 # Turn on bash strict mode.
 set -euxo pipefail
 
-target_folder=\{\{cookiecutter.repo\}\}
+cookiecutter_dir=\{\{cookiecutter.repo\}\}
 
 # Delete dep files.
-rm -rf $target_folder/requirements-*.txt
+rm -rf $cookiecutter_dir/requirements-*.txt
 
 # Install dev dependencies.
 pip install pip-tools
 
 # Update the deps.
-pip-compile $target_folder/requirements.in -o $target_folder/requirements.txt &&\
-pip-compile $target_folder/requirements-dev.in -o $target_folder/requirements-dev.txt
+pip-compile $cookiecutter_dir/requirements.in \
+-o $cookiecutter_dir/requirements.txt &&\
+
+pip-compile $cookiecutter_dir/requirements-dev.in \
+-o $cookiecutter_dir/requirements-dev.txt
