ce6d565162d18c795be8ada3a7ba086c9ba8e42f
Redowan Delowar <redowan.nafi@gmail.com>
2021-09-23 04:25:12 +0600

    Fixed the tests

--- app/core/config.py
+++ app/core/config.py
@@ -4,23 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv("./.env")
 
-ENV_STATE = os.environ["ENV_STATE"]
 
 # Base configs.
 HOST = os.environ["HOST"]
+PORT = os.environ["PORT"]
 
-
-# Prod configs.
-if ENV_STATE == "prod":
-    API_USERNAME = os.environ["PROD_API_USERNAME"]
-    API_PASSWORD = os.environ["PROD_API_PASSWORD"]
-
-
-# Dev configs.
-else:
-    API_USERNAME = os.environ["DEV_API_USERNAME"]
-    API_PASSWORD = os.environ["DEV_API_PASSWORD"]
-
+API_USERNAME = os.environ["API_USERNAME"]
+API_PASSWORD = os.environ["API_PASSWORD"]
 
 # Auth configs.
 API_SECRET_KEY = os.environ["API_SECRET_KEY"]
--- app/routes/views.py
+++ app/routes/views.py
@@ -6,7 +6,7 @@ from app.apis.api_a.mainmod import main_func as main_func_a
 from app.apis.api_b.mainmod import main_func as main_func_b
 from app.core.auth import get_current_user
 
-router = APIRouter(prefix="/api/v1")
+router = APIRouter()
 
 
 @router.get("/api_a/{num}", tags=["api_a"])
